a3f93bcc7dbd3918c356076e56072690a038e3a3
Arfanur Rahman <arfanur36@gmail.com>
2023-03-16 20:03:18 +0600

    2-7

--- index.js
+++ index.js
@@ -7,7 +7,26 @@ const other = require("./other");
 
 const http = require("http");
 const server = http.createServer((req, res) => {
-	res.end("Welcome to Full Stack Development");
+	// console.log(req.url);
+	if (req.url == "/") {
+		res.writeHead(200, { "Content-type": "text/html" });
+		res.write("<p>Welcome to Full Stack Development</p>");
+		res.end();
+	} else if (req.url == "/app-json") {
+		res.writeHead(200, { "Content-type": "application/json" });
+		res.write(JSON.stringify({ course: "ACC" }));
+		res.end();
+	} else if (req.url == "/contact") {
+		res.writeHead(200, { "Content-type": "text/html" });
+		res.write("<p>This is Contact Page</p>");
+		res.end();
+	}
+	if (req.url == "/about-us") {
+		res.writeHead(200, { "Content-type": "text/html" });
+		res.write("<p>This is About Us Page</p>");
+		res.end();
+	}
+	// res.end("Welcome to Full Stack Development");
 });
 const PORT = 5000;
 server.listen(PORT);
