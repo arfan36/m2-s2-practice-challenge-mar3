da613df8f5d32e451336186f8a513d0dce4f95ba
Arfanur Rahman <arfanur36@gmail.com>
2023-03-17 11:37:29 +0600

    2-8

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-// local module
-const other = require("./other");
-// console.log("🚀 ~ other:", other);
-
-// const res = other.subtract(5, 4);
-// console.log("🚀 ~ res:", res);
-
+// const other = require("./other");
 const http = require("http");
+const url = require("url");
+
 const server = http.createServer((req, res) => {
-	// console.log(req.url);
-	if (req.url == "/") {
-		res.writeHead(200, { "Content-type": "text/html" });
-		res.write("<p>Welcome to Full Stack Development</p>");
-		res.end();
-	} else if (req.url == "/app-json") {
-		res.writeHead(200, { "Content-type": "application/json" });
-		res.write(JSON.stringify({ course: "ACC" }));
-		res.end();
-	} else if (req.url == "/contact") {
-		res.writeHead(200, { "Content-type": "text/html" });
-		res.write("<p>This is Contact Page</p>");
-		res.end();
-	}
-	if (req.url == "/about-us") {
-		res.writeHead(200, { "Content-type": "text/html" });
-		res.write("<p>This is About Us Page</p>");
-		res.end();
-	}
-	// res.end("Welcome to Full Stack Development");
+	const address_url = `http://localhost:5000/contact?name=mezba&country=bangladesh`;
+
+	const parsed_url = url.parse(address_url, true);
+	const queryObject = parsed_url.query;
+	console.log("🚀 ~ queryObject:", queryObject);
 });
+
+// console.log(url);
 const PORT = 5000;
 server.listen(PORT);
-// console.log("🚀 ~ http:", http);
 console.log(`server is running at ${PORT}`);
