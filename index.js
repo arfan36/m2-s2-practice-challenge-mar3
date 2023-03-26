f6179f531ce03a12308280d577c14c54d253df78
Arfanur Rahman <arfanur36@gmail.com>
2023-03-26 12:23:39 +0600

    2-10

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
-// const other = require("./other");
-const http = require("http");
-// const url = require("url");
-const fs = require("fs");
+const events = require("events");
+const eventEmitter = new events.EventEmitter();
 
-const server = http.createServer((req, res) => {
-	if ((req.url = "/")) {
-		// fs.readFile("data.txt", (err, data) => {
-		// 	if (err) {
-		// 		res.write("Failed to read data !!!");
-		// 		res.end();
-		// 	} else {
-		// 		res.write(data);
-		// 		res.end();
-		// 	}
-		// });
+// creating an event handler
+const chitkarDibo = () => {
+	console.log("oi beta koi tui??? ami darai achi");
+};
 
-		// const data = fs.readFileSync("data.txt");
-		// res.write(data);
-		// res.end();
+// assign the handler into an event
+eventEmitter.on("scream", chitkarDibo).chitkarDibo;
 
-		fs.writeFile("first.txt", "I am a pull stack developer", (err) => {
-			if (err) {
-				res.write("Data failed to write");
-				res.end();
-			} else {
-				res.write("data written successfully");
-				res.end();
-			}
-		});
-	}
-});
-
-// console.log(url);
-const PORT = 5000;
-server.listen(PORT);
-console.log(`server is running at ${PORT}`);
+// firing the event
+eventEmitter.emit("scream");
